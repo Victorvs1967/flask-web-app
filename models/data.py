cdb3fc1b9d76b2720f7dee2c9449c7c0b06692b2
Victor <victorsmirnov@me.com>
2020-05-21 00:30:57 +0300

    edit model

--- models/data.py
+++ models/data.py
@@ -4,8 +4,8 @@ from app import app
 
 
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://victors:victor77@localhost/FlaskData'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://hozozwjzrkzdgx:646ad922cebc4582557014d371a0b8b1854e1806b6e65b1df0194133759cafd6@ec2-46-137-84-140.eu-west-1.compute.amazonaws.com:5432/d8qvcrgebn06fa
-'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://hozozwjzrkzdgx:646ad922cebc4582557014d371a0b8b1854e1806b6e65b1df0194133759cafd6@ec2-46-137-84-140.eu-west-1.compute.amazonaws.com:5432/d8qvcrgebn06fa'
+
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
